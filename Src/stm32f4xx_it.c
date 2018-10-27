1fa72edaf9f5711d115d421acd28e7706bdc2808
saket kirange <SAKET71193@GMAIL.COM>
2018-10-27 11:09:13 +0530

    tested ok

--- Src/stm32f4xx_it.c
+++ Src/stm32f4xx_it.c
@@ -30,78 +30,78 @@
   *
   ******************************************************************************
   */
-/* Includes ------------------------------------------------------------------*/
-#include "stm32f4xx_hal.h"
-#include "stm32f4xx.h"
-#include "stm32f4xx_it.h"
+			/* Includes ------------------------------------------------------------------*/
+			#include "stm32f4xx_hal.h"
+			#include "stm32f4xx.h"
+			#include "stm32f4xx_it.h"
 
-/* USER CODE BEGIN 0 */
-volatile uint8_t FatFsCnt = 0;
-volatile uint8_t Timer1, Timer2;
+			/* USER CODE BEGIN 0 */
+			volatile uint8_t FatFsCnt = 0;
+			volatile uint8_t Timer1, Timer2;
 
-void SDTimer_Handler(void)
-{  
-  if(Timer1 > 0)
-    Timer1--;
-  
-  if(Timer2 > 0)
-    Timer2--;
-}
+			void SDTimer_Handler(void)
+			{  
+				if(Timer1 > 0)
+					Timer1--;
+				
+				if(Timer2 > 0)
+					Timer2--;
+			}
 
-/* USER CODE END 0 */
+			/* USER CODE END 0 */
 
-/* External variables --------------------------------------------------------*/
+			/* External variables --------------------------------------------------------*/
 
-/******************************************************************************/
-/*            Cortex-M3 Processor Interruption and Exception Handlers         */ 
-/******************************************************************************/
+			/******************************************************************************/
+			/*            Cortex-M3 Processor Interruption and Exception Handlers         */ 
+			/******************************************************************************/
 
-/**
-* @brief This function handles System service call via SWI instruction.
-*/
-void SVC_Handler(void)
-{
-  /* USER CODE BEGIN SVCall_IRQn 0 */
+			/**
+			* @brief This function handles System service call via SWI instruction.
+			*/
+			void SVC_Handler(void)
+			{
+				/* USER CODE BEGIN SVCall_IRQn 0 */
 
-  /* USER CODE END SVCall_IRQn 0 */
-  /* USER CODE BEGIN SVCall_IRQn 1 */
+				/* USER CODE END SVCall_IRQn 0 */
+				/* USER CODE BEGIN SVCall_IRQn 1 */
 
-  /* USER CODE END SVCall_IRQn 1 */
-}
+				/* USER CODE END SVCall_IRQn 1 */
+			}
 
-/**
-* @brief This function handles Pendable request for system service.
-*/
-void PendSV_Handler(void)
-{
-  /* USER CODE BEGIN PendSV_IRQn 0 */
+			/**
+			* @brief This function handles Pendable request for system service.
+			*/
+			void PendSV_Handler(void)
+			{
+				/* USER CODE BEGIN PendSV_IRQn 0 */
 
-  /* USER CODE END PendSV_IRQn 0 */
-  /* USER CODE BEGIN PendSV_IRQn 1 */
+				/* USER CODE END PendSV_IRQn 0 */
+				/* USER CODE BEGIN PendSV_IRQn 1 */
 
-  /* USER CODE END PendSV_IRQn 1 */
-}
+				/* USER CODE END PendSV_IRQn 1 */
+			}
 
-/**
-* @brief This function handles System tick timer.
-*/
-void SysTick_Handler(void)
-{
-  /* USER CODE BEGIN SysTick_IRQn 0 */
-	FatFsCnt++;
-  if(FatFsCnt >= 10)
-  {
-    FatFsCnt = 0;
-    SDTimer_Handler();
-  }  
+			/**
+			* @brief This function handles System tick timer.
+			*/
+			void SysTick_Handler(void)
+			{
+				/* USER CODE BEGIN SysTick_IRQn 0 */
+				FatFsCnt++;
+				if(FatFsCnt >= 10)
+				{
+					FatFsCnt = 0;
+					SDTimer_Handler();
+				}  
 
-  /* USER CODE END SysTick_IRQn 0 */
-  HAL_IncTick();
-  HAL_SYSTICK_IRQHandler();
-  /* USER CODE BEGIN SysTick_IRQn 1 */
+				/* USER CODE END SysTick_IRQn 0 */
+				HAL_IncTick();
+				HAL_SYSTICK_IRQHandler();
+				/* USER CODE BEGIN SysTick_IRQn 1 */
 
-  /* USER CODE END SysTick_IRQn 1 */
-}
+				/* USER CODE END SysTick_IRQn 1 */
+			}
 
 /******************************************************************************/
 /* STM32F1xx Peripheral Interrupt Handlers                                    */
